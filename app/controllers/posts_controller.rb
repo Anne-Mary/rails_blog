d12fd6aeaa14b2ac00d94395905dde87c0aa1b31
Tech-A32 <Tech-A32@Tech-A32s-MacBook-Air.local>
2017-07-19 14:34:39 +0100

    Added titles and social media links and a nike logo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all.order("title")
 	end
 
 	def new
+		
 	end
+
+	def create
+		@post = Post.create(post_params)
+		@post.user_id = current_user.id
+		@post.date = Time.now
+		@post.save 
+
+		redirect_to @post  
+	end 
+
+	def show 
+		@post = Post.find(params[:id])
+	end
+
+	private 
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
+
 end
