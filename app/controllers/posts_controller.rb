29a530cc87cae0514c4a2c68204122e2bdcee300
Tech-A32 <Tech-A32@Tech-A32s-MacBook-Air.local>
2017-07-21 12:04:19 +0100

    blog working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,7 @@
 class PostsController < ApplicationController
 	def index
-		@posts = Post.all.order("title")
+		@posts = Post.all
 	end
-
-	def new
-		@post = Post.new
-	end
-
-	def create
-		@post = Post.create(post_params)
-		@post.user_id = current_user.id
-		@post.date = Time.now
-		@post.save 
-
-		redirect_to @post  
-	end 
 
 	def show 
 		@post = Post.find(params[:id])
@@ -30,10 +17,24 @@ class PostsController < ApplicationController
 		redirect_to @post
 	end
 
+	def new
+		@post = Post.new
+	end
+
+
+	def create
+		@post = Post.create(post_params)
+		@post.user_id = current_user.id
+		@post.date = Time.now
+		@post.save 
+
+		redirect_to @post  
+	end 
+
 	def destroy
 		@post = Post.find(params[:id])
 		@post.destroy
-		redirect_to post_path
+		redirect_to posts_path
 	end
 
 	private 
