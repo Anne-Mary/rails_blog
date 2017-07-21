66fd46137c8c6c39774f34cf98bcc941ca19b747
Tech-A32 <Tech-A32@Tech-A32s-MacBook-Air.local>
2017-07-21 10:48:28 +0100

    created and edit and delete for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 	end
 
 	def new
-		
+		@post = Post.new
 	end
 
 	def create
@@ -18,6 +18,22 @@ class PostsController < ApplicationController
 
 	def show 
 		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to @post
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
 	end
 
 	private 
